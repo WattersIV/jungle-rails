d88e2293a598dba29e13db4b2c70a3d57b3f3c41
Bill Watters <billywatters13@gmail.com>
2020-11-17 21:19:41 +0000

    Add home page testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,3 @@
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist"
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -11,6 +8,13 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist" # Add this line to require poltergeist
+
+# Specs flagged with `js: true` will use Capybara's JS driver. Set
+# that JS driver to :poltergeist
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -63,5 +67,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  Capybara.javascript_driver = :poltergeist
 end
